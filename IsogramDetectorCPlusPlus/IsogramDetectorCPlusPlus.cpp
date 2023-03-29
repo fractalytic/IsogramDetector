2831f39d86ef04800e8bc2c1d2c4619b35efdbe0
Fractalytic <shanekennedy@blueyonder.co.uk>
2023-03-30 00:53:55 +0100

    Added in capability for multiple attempts

--- IsogramDetectorCPlusPlus/IsogramDetectorCPlusPlus.cpp
+++ IsogramDetectorCPlusPlus/IsogramDetectorCPlusPlus.cpp
@@ -37,65 +37,83 @@ int main()
     bool punctuationFound = false;
     int letterArray[26] = {};
     bool isogram = true;
+    bool continuePlaying = true;
+    string loopAnswer = "n";
     clock_t startTime, currentTime;
     double elapsedSeconds;
 
-    // Get the input word from the user
-    while (validInput == false) {
-        std::cout << "Please enter a single word to test > ";
-        getline(cin, inputString);
+    while (continuePlaying == true) {
+        
+        validInput = false;
+        // Get an input word from the user
+        while (validInput == false) {
+            std::cout << "Please enter a single word to test > ";
+            getline(cin, inputString);
 
-        // Test to see if this is a single word (no spaces), without numbers or punctuation marks
-        spaceFound = false;
-        digitFound = false;
-        punctuationFound = false;
+            // Test to see if this is a single word (no spaces), without numbers or punctuation marks
+            spaceFound = false;
+            digitFound = false;
+            punctuationFound = false;
+            for (int i = 0; i < inputString.length(); i++) {
+                if (isspace(inputString[i])) {
+                    spaceFound = true;
+                }
+                if (isdigit(inputString[i])) {
+                    digitFound = true;
+                }
+                if (ispunct(inputString[i])) {
+                    punctuationFound = true;
+                }
+            }
+            validInput = not(spaceFound || digitFound || punctuationFound);
+
+            if (spaceFound) {
+                cout << "\nThat word isn't valid as it contains a space, please try again\n";
+            }
+            else if (digitFound) {
+                cout << "\nThat word isn't valid as it contains a number, please try again\n";
+            }
+            else if (punctuationFound) {
+                cout << "\nThat word isn't valid as it contains a punctuation mark, please try again\n";
+            }
+
+        }
+
+        // Split word into component letters and run through each
+        for (int i = 0; i < 26; i++) {
+            letterArray[i] = 0;
+        }
+        isogram = true;
         for (int i = 0; i < inputString.length(); i++) {
-            if (isspace(inputString[i])) {
-                spaceFound = true;
-            }
-            if (isdigit(inputString[i])) {
-                digitFound = true;
-            }
-            if (ispunct(inputString[i])) {
-                punctuationFound = true;
+            letterArray[(int)tolower(inputString[i]) - 97]++;
+            if (letterArray[(int)tolower(inputString[i]) - 97] > 1) {
+                isogram = false;
+                break;
             }
         }
-        validInput = not(spaceFound || digitFound || punctuationFound);
 
-        if (spaceFound) {
-            cout << "\nThat word isn't valid as it contains a space, please try again\n";
+        // If a letter is used more than one time return false
+        if (isogram) {
+            cout << "Yes, congratulations the word you entered (" << inputString << ") is an isogram!" << endl << endl;
         }
-        else if (digitFound) {
-            cout << "\nThat word isn't valid as it contains a number, please try again\n";
-        }
-        else if (punctuationFound) {
-            cout << "\nThat word isn't valid as it contains a punctuation mark, please try again\n";
+        else {
+            cout << "No, sorry but the word you entered (" << inputString << ") is not an isogram as it has letters that appear more than once" << endl << endl;
         }
 
-    }
-
-    // Split word into component letters and run through each
-    for (int i = 0; i < inputString.length(); i++) {
-        letterArray[(int) tolower(inputString[i]) - 97]++;
-        if (letterArray[(int) tolower(inputString[i]) - 97] > 1) {
-            isogram = false;
-            // TODO: break from here if isogram = false
+        // TODO: Add in timing
+        // TODO: Ask if user wants to try again
+        cout << "Do you want another go (Y/N)? ";
+        getline(cin, loopAnswer);
+        if (loopAnswer == "y" || loopAnswer == "Y") {
+            continuePlaying = true;
+            cout << endl;
         }
+        else {
+            continuePlaying = false;
+        }
+
+
     }
-
-    // If a letter is used more than one time return false
-    if (isogram) {
-        cout << "Yes, congratulations the word you entered (" << inputString << ") is an isogram!";
-    }
-    else {
-        cout << "No, sorry but the word you entered (" << inputString << ") is not an isogram as it has letters that appear more than once";
-    }
-
-    // TODO: Ask if user wants to try again
-
-    
-
-    
 }
 
 
